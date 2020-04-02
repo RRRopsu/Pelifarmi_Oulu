b7f9a37109dff49a70c4613bfead9ac4f524984a
unknown <henrihuhtala@kamk.fi>
2020-04-02 12:09:23 +0300

    rajahdys salamejaaarghh

---
+++ Assets/Scripts/ParticleExplosion.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleExplosion : MonoBehaviour
+{
+    public float rate = 0.1f;
+    public AudioClip explosion;
+
+    ParticleSystem particle;
+    float timer;
+    AudioSource audio;
+
+    private void Start()
+    {
+        particle = gameObject.GetComponent<ParticleSystem>();
+        audio = gameObject.GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (timer > rate)
+            {
+                particle.Emit(1);
+                timer = 0;
+            }
+        }
+    }
+
+    void OnParticleCollision(GameObject col)
+    {
+        Camera.main.GetComponent<ScreenShake>().ShakeScreen();
+        audio.pitch = Random.Range(0.90f, 1.10f);
+        audio.PlayOneShot(explosion, Random.Range(0.2f, 0.4f));
+    }
+}
---
+++ Assets/Scripts/ScreenShake.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenShake : MonoBehaviour
+{
+
+    Coroutine routine;
+    float startFov = 0;
+    private void Start()
+    {
+        startFov = Camera.main.fieldOfView;
+    }
+
+    public void ShakeScreen()
+    {
+        if (routine != null)
+        {
+            StopCoroutine(routine);
+        }
+        Camera.main.fieldOfView = startFov;
+        routine = StartCoroutine(Shake());
+    }
+
+    IEnumerator Shake()
+    {
+        float t = 0;
+        float size = Camera.main.fieldOfView;
+        Camera.main.fieldOfView -= 1.5f;
+
+        while (t < 0.6f)
+        {
+            Camera.main.fieldOfView = Mathf.Lerp(Camera.main.fieldOfView, size, 5 * Time.deltaTime);
+            t += Time.deltaTime;
+            yield return null;
+        }
+        Camera.main.fieldOfView = size;
+        yield return null;
+    }
+}
