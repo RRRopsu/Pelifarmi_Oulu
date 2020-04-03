39ed95281dd73fde089479eacc5ad3c7c7004a47
Mikko Kropsu <mikko.kropsu@kaamosgames.com>
2020-04-03 22:16:45 +0300

    Cleaned up project a little bit

---
+++ Assets/RandomFarmingByPelifarmers/salami/kikkailuskripti.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kikkailuskripti : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Tämä file pitää ignoroida
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
